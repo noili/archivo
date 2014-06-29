f991bf51eeb0cb31b0d6d27ecc0a4d7d8ef9ef4d
noili <federico.neuman@gmail.com>
2014-06-29 16:58:39 -0300

    Agregado welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'offices/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'offices#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
