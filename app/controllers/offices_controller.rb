e6e9094729a9aeca2b788d0cfd5c8f9357d9581c
noili <federico.neuman@gmail.com>
2014-06-29 18:37:00 -0300

    Definidos new, create, show

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,2 +1,25 @@
 class OfficesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @office = Office.new(office_params)
+
+    if @office.save
+      redirect_to @office
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @office = Office.find(params[:id])
+  end
+  
+  private
+    def office_params
+      params.require(:office).permit(:name)
+    end
+  
 end
