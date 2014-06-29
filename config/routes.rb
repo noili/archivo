cac4ef8169c7473275482aa8d4ee62255a44a11c
noili <federico.neuman@gmail.com>
2014-06-29 13:12:41 -0300

    Agregador Office

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :people
+  resources :offices
 
   # Example resource route with options:
   #   resources :products do
