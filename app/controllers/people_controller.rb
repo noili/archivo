c3793671526c32531fbc939fe9f889991bb26676
noili <federico.neuman@gmail.com>
2014-06-29 16:20:17 -0300

    agregado new, create en people_controller. Corregido db

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,19 @@
 class PeopleController < ApplicationController
+  
+  def new
+    @person = Person.new
+  end
+  
+  def create
+    @person = Person.new person_params
+    if @person.save
+      puts 'salvado'
+    else
+      puts 'error al salvar'
+  end
+  
+  def index
+    @people = Person.all
+  end
+  
 end
--- db/migrate/20140628173545_create_people.rb
+++ db/migrate/20140628173545_create_people.rb
@@ -2,7 +2,7 @@ class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
       t.string :name
-      t.Integer :dni
+      t.integer :dni
 
       t.timestamps
     end
