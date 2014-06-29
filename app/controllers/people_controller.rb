514e8bda2facbf6eabe293049b0a9cd57c553289
noili <federico.neuman@gmail.com>
2014-06-29 17:05:01 -0300

    agregado edit(no funciona) y view para new

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,9 @@
 class PeopleController < ApplicationController
-  
+
   def new
     @person = Person.new
   end
-  
+
   def create
     @person = Person.new person_params
     if @person.save
@@ -11,19 +11,23 @@ class PeopleController < ApplicationController
     else
       puts 'error al salvar'
   end
-  
+
   def index
     @people = Person.all
   end
-  
+
   def show
     @person = Person.find(params[:id])
   end
-  
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
   private
-  
+
     def person_params
       params.require(:person).permit(:name, :dni)
     end
-  
+
 end
