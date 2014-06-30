e16443e07b74add40601dcefe4ac2d615961843f
noili <federico.neuman@gmail.com>
2014-06-29 22:56:06 -0300

    terminado CRUD para people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,22 +6,41 @@ class PeopleController < ApplicationController
 
   def create
     @person = Person.new person_params
-    if @person.save
-      puts 'salvado'
-    else
-      puts 'error al salvar'
-  end
 
-  def index
-    @people = Person.all
+    if @person.save
+      redirect_to @person
+    else
+      render 'new'
+    end
   end
 
   def show
     @person = Person.find(params[:id])
   end
 
+  def index
+    @people = Person.all
+  end
+
   def edit
     @person = Person.find(params[:id])
+  end
+  
+  def update
+    @person = Person.find params[:id]
+ 
+    if @person.update person_params
+      redirect_to @person
+    else
+      render 'edit'
+    end    
+  end
+  
+  def destroy
+    @person = Person.find params[:id]
+    @person.destroy
+    
+    redirect_to people_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
   
+  get 'people/index'
+  
   resources :offices
+  resources :people
   #get 'offices/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
