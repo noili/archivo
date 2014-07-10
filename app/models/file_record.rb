f9282271ba96a2b03b00b99c7bb74542d33eb9bb
noili <federico.neuman@gmail.com>
2014-07-10 15:54:10 -0300

    agregado start y archivate

--- app/models/file_record.rb
+++ app/models/file_record.rb
@@ -1,10 +1,24 @@
 class FileRecord < ActiveRecord::Base
   has_many :steps
   
+  def archivate
+    step = Step.archivate
+    steps.create office: step.office
+  end
+  
   def save_step
     office = Office.last!
     steps.create! office: office
   end
   
-  after_save :save_step
+  def create_step
+    office = Office.first!
+    steps.create office: office
+  end
+  
+  def create
+    step.start
+  end
+  
+  after_create :create_step
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,17 @@
 class Step < ActiveRecord::Base
+
   belongs_to :office
   belongs_to :person
   belongs_to :file_record
+  
+  def self.start
+    office = Office.find_or_create_by name: 'Mesa de entrada'
+    step = step.new office
+  end
+  
+  def self.archivate
+    office = Office.find_or_create_by name: 'Archivo'
+    step = step.new office
+  end
+  
 end
