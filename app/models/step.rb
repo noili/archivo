360945141e804ae8e440a25612b99907a6921358
noili <federico.neuman@gmail.com>
2014-07-17 18:13:36 -0300

    Removed unused code.

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,12 +3,7 @@ class Step < ActiveRecord::Base
   belongs_to :office
   belongs_to :person
   belongs_to :file_record
-  
-  def self.start
-    office = Office.find_or_create_by name: 'Mesa de Entrada'
-    step = step.new office
-  end
-  
+
   def self.archivate
     create office: Office.archive_office
   end
