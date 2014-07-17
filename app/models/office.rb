03a97f41a92d185049935a2bddd1e2f0cc266fa6
noili <federico.neuman@gmail.com>
2014-07-17 18:13:16 -0300

    Corrected upcase.

--- app/models/office.rb
+++ app/models/office.rb
@@ -3,7 +3,7 @@ class Office < ActiveRecord::Base
   has_many :steps
   
   def self.start_office
-    Office.find_or_create_by name: 'Mesa de entrada'
+    Office.find_or_create_by name: 'Mesa de Entrada'
   end
   
   validates :name, presence: true,
