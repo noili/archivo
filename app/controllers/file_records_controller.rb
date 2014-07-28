fe823971aa6530e4cd68140ffcda2f05431cb1d2
noili <federico.neuman@gmail.com>
2014-07-28 00:34:17 -0300

    Added inherited_resources.

--- app/controllers/file_records_controller.rb
+++ app/controllers/file_records_controller.rb
@@ -1,54 +1,11 @@
-class FileRecordsController < ApplicationController
+class FileRecordsController < InheritedResources::Base
 
-  def new
-    @file_record = FileRecord.new
-  end
-
-  def create
-    @file_record = FileRecord.new file_record_params
-
-    if @file_record.save
-      redirect_to @file_record
-    else
-      render 'new'
-    end
-  end
-
-  def show
-    @file_record = FileRecord.find(params[:id])
-    @step = Step.new
-  end
-
-  def index
-    @file_records = FileRecord.all
-  end
-
-  def edit
-    @file_record = FileRecord.find(params[:id])
-  end
-  
-  def update
-    @file_record = FileRecord.find params[:id]
- 
-    if @file_record.update file_record_params
-      redirect_to @file_record
-    else
-      render 'edit'
-    end    
-  end
   
   def archivate
     @file_record = FileRecord.find(params[:file_record_id]).archivate
     redirect_to file_records_path
   end
   
-  def destroy
-    @file_record = FileRecord.find params[:id]
-    @file_record.destroy
-    
-    redirect_to file_records_path
-  end
-
   private
 
     def file_record_params
