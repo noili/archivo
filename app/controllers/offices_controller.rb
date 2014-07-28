9eb46d7fa0e14b32ed0360b30702ce4d7ff68798
noili <federico.neuman@gmail.com>
2014-07-28 00:44:41 -0300

    Added inherited_resources.

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,51 +1,8 @@
-class OfficesController < ApplicationController
+class OfficesController < InheritedResources::Base
 
-  def new
-    @office = Office.new
-  end
-
-  def create
-    @office = Office.new office_params
-
-    if @office.save
-      redirect_to @office
-    else
-      render 'new'
-    end
-  end
-  
-  def show
-    @office = Office.find params[:id]
-  end
-  
-  def index
-    @offices = Office.all
-  end
-  
-  def edit
-    @office = Office.find(params[:id])
-  end
-  
-  def update
-    @office = Office.find params[:id]
- 
-    if @office.update office_params
-      redirect_to @office
-    else
-      render 'edit'
-    end    
-  end
-  
-  def destroy
-    @office = Office.find params[:id]
-    @office.destroy
-    
-    redirect_to offices_path
-  end
-  
   private
     def office_params
       params.require(:office).permit(:name)
     end
-  
+
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,47 +1,4 @@
-class PeopleController < ApplicationController
-
-  def new
-    @person = Person.new
-  end
-
-  def create
-    @person = Person.new person_params
-
-    if @person.save
-      redirect_to @person
-    else
-      render 'new'
-    end
-  end
-
-  def show
-    @person = Person.find params[:id]
-  end
-
-  def index
-    @people = Person.all
-  end
-
-  def edit
-    @person = Person.find params[:id]
-  end
-  
-  def update
-    @person = Person.find params[:id]
- 
-    if @person.update person_params
-      redirect_to @person
-    else
-      render 'edit'
-    end    
-  end
-  
-  def destroy
-    @person = Person.find params[:id]
-    @person.destroy
-    
-    redirect_to people_path
-  end
+class PeopleController < InheritedResources::Base
 
   private
 
