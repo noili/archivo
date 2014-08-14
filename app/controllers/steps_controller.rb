ae863c1b10991a3be1e01b7b89e4257302019b64
noili <federico.neuman@gmail.com>
2014-08-14 20:14:08 -0300

    Corrected parameters.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -2,12 +2,12 @@ class StepsController < ApplicationController
 
   def create
     @file_record = FileRecord.find(params[:file_record_id])
-    person = Person.find(params[:step][:person])
+    person = Person.find(params[:step][:person_id])
     if person.completed?
       @file_record.steps.create(step_params)
       redirect_to file_record_path(params[:file_record_id])
     else
-      redirect_to edit_person_path(params[:step][:person])
+      redirect_to edit_person_path(params[:step][:person_id])
     end
   end
   
