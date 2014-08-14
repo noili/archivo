a4dd64509de0e296138891ef36354575008a5d22
noili <federico.neuman@gmail.com>
2014-08-14 20:15:29 -0300

    Test 'cuando la persona incompleta 2'.

--- test/controllers/step_controller_test.rb
+++ test/controllers/step_controller_test.rb
@@ -10,4 +10,12 @@ class StepsControllerTest < ActionController::TestCase
     assert_redirected_to edit_person_path step.person
   end
   
+  test 'cuando la persona' do
+    file_record = file_records(:frejemplo)
+    step_hash = {email: 'emmet@mail.com', file_record_id: file_record.id}
+    person = Person.find_by_email 'emmet@mail.com'
+    post :create, step: step_hash
+    assert_redirected_to edit_person_path
+  end
+  
 end
